0d25945ea29c73868fc51fd42c73edb886f0ef21
dmacuk <dmacuk@blueyonder.co.uk>
2015-06-19 21:16:19 +0100

    Started FileUtils.WriteObject

---
+++ Utils/Object/ObjectUtils.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace DMSoftware.Object
+{
+    public class ObjectUtils
+    {
+        public string Serialise<T>(T t, bool encrypt)
+        {
+            var json = JsonConvert.SerializeObject(t);
+            return json;
+        }
+    }
+}
---
+++ Utils/String/StringUtils.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace DMSoftware.String
+{
+    public static class StringUtils
+    {
+    }
+}
