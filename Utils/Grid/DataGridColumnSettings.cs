2602082313060ef649c5cfbf0fad4998d590f5aa
dmacuk <dmacuk@blueyonder.co.uk>
2015-08-30 19:24:58 +0100

    DataGridColumnSettings: Now passes/gets  List<ColumnProperty> bypassing the needs to Json deserilaise. Added PreferenceManager and StringExtensions.

--- Utils/Grid/DataGridColumnSettings.cs
+++ Utils/Grid/DataGridColumnSettings.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using Newtonsoft.Json;
+using Utils.File;
 
 namespace Utils.Grid
 {
@@ -14,28 +15,25 @@ namespace Utils.Grid
     /// </summary>
     public static class DataGridColumnSettings
     {
-
         /// <summary>
         /// Get the supplied Datagrid's column settings
         /// </summary>
         /// <param name="dg">A DataGrid</param>
         /// <returns></returns>
-        public static string GetColumOptions(this DataGrid dg)
+        public static List<ColumnProperty> GetColumOptions(this DataGrid dg)
         {
-            var columnProperties = dg.Columns.Select(column => new ColumnProperty(column.DisplayIndex, column.Width.DisplayValue, column.Visibility, column.SortDirection)).ToList();
-            return JsonConvert.SerializeObject(columnProperties);
+            return dg.Columns.Select(column => new ColumnProperty(column.DisplayIndex, column.Width.DisplayValue, column.Visibility, column.SortDirection)).ToList();
         }
 
         /// <summary>
         /// Set the passed DataGrid's columns settings based on the passed XML
         /// </summary>
         /// <param name="dg">A DataGrid</param>
-        /// <param name="json">Column settings for the passed DataGrid. This is usually the String returned from <strong>GetColumOption</strong></param>
-        public static void SetColumnOptions(this DataGrid dg, string json)
+        /// <param name="columnProperties"></param>
+        public static void SetColumnOptions(this DataGrid dg, List<ColumnProperty> columnProperties)
         {
             try
             {
-                var columnProperties = JsonConvert.DeserializeObject<List<ColumnProperty>>(json);
                 var columnCount = 0;
                 foreach (var columnProperty in columnProperties)
                 {
@@ -50,11 +48,9 @@ namespace Utils.Grid
                     }
                     columnCount++;
                 }
-
             }
             catch (Exception e)
             {
-                
                 Debug.WriteLine(e.Message);
             }
         }
@@ -63,9 +59,10 @@ namespace Utils.Grid
     /// <summary>
     /// Holder for various DataGrid column properties
     /// </summary>
-    class ColumnProperty
+    public class ColumnProperty
     {
 #pragma warning disable 1591
+
         public ColumnProperty(int displayIndex, double width, Visibility visibility, ListSortDirection? listSortDirection)
         {
             DisplayIndex = displayIndex;
@@ -79,16 +76,5 @@ namespace Utils.Grid
         public Visibility Visibility { get; set; }
         public ListSortDirection? ListSortDescription { get; set; }
 #pragma warning restore 1591
-
-
-
-        /*
-         *             colTable.Columns.Add(ColDisplayindex, typeof (Int32));
-            colTable.Columns.Add(ColWidth, typeof (double));
-            colTable.Columns.Add(ColVisibility, typeof (Int32));
-            colTable.Columns.Add(ColSortdirection, typeof (Int32));
-
-         */
-
     }
 }
---
+++ Utils/Preference/PreferenceManager.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Utils.File;
+
+namespace Utils.Preference
+{
+    public sealed class PreferenceManager
+    {
+        private static Dictionary<string, string> _preferences;
+        private static JsonUtils<Dictionary<string, string>> _jsonUtils;
+        private static string _fileName;
+        private static PreferenceManager _instance;
+
+        private PreferenceManager(string fileName)
+        {
+            _fileName = fileName;
+            _jsonUtils = new JsonUtils<Dictionary<string, string>>(_fileName);
+        }
+
+        public static PreferenceManager GetInstance(string fileName)
+        {
+            if (_instance == null)
+            {
+                _instance = new PreferenceManager(fileName);
+                LoadPreferences();
+            }
+            return _instance;
+        }
+
+        public static PreferenceManager GetInstance()
+        {
+            if (_fileName == null) throw new Exception("Preference manager not initialised");
+            return _instance;
+        }
+
+
+        private static async void LoadPreferences()
+        {
+            if (System.IO.File.Exists(_fileName))
+            {
+                _preferences = await _jsonUtils.ReadObjectAsync();
+
+            }
+            else
+            {
+                _preferences = new Dictionary<string, string>();
+            }
+
+        }
+
+        public async void SavePreferences()
+        {
+            await _jsonUtils.WriteObjectAsync(_preferences);
+        }
+
+        public static T GetPreference<T>(string name, T defaultValue)
+        {
+            if (_preferences == null) throw new PreferenceException("Preferences not loaded");
+            if (!_preferences.ContainsKey(name)) return defaultValue;
+            var value = _preferences[name];
+            return JsonUtils<T>.ReadJsonString(value);
+        }
+
+        public static void SetPreference<T>(string name, T value)
+        {
+            var json = JsonUtils<T>.WriteJsonString(value);
+            _preferences[name] = json;
+        }
+    }
+
+    public class PreferenceException : Exception
+    {
+        public PreferenceException(string message):base(message)
+        {
+        }
+    }
+}
