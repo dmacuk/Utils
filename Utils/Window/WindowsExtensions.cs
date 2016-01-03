690bb854ec524488e02936455c146625983d275d
dmacuk <dmacuk@blueyonder.co.uk>
2016-01-03 18:18:53 +0000

    Instance saver/loader for windows

---
+++ Utils/Window/WindowsExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Utils.Window.Utils;
+
+namespace Utils.Window
+{
+    public static class WindowsExtensions
+    {
+        public static void SaveSettings(this System.Windows.Window window)
+        {
+            var saver = new WindowContentSaver(window);
+            saver.Save();
+        }
+
+        public static void LoadSettings(this System.Windows.Window window)
+        {
+            var loader = new WindowContentLoader(window);
+            loader.Load();
+        }
+    }
+}
